db9012b21190542f53ba95f97f42eb4778324e06
MacCampbell <campbellmatthewa@gmail.com>
2019-09-07 21:24:06 -0800

    Adding missing data thresh.

--- 102.1-calculate-PCA.sh
+++ 102.1-calculate-PCA.sh
@@ -24,8 +24,8 @@ mkdir /home/macampbell2/pike-time/outputs/102
 # ANGSD can be easily be used to compute genotype likelihoods and output them in the
 # required Beagle format {says the manual}.
 
-#Do the calculations (putting in absolute paths)
-/home/macampbell2/angsd/angsd -GL 1 -out /home/macampbell2/pike-time/outputs/102/197 -nThreads 24 -doGlf 2 -doMajorMinor 1  -doMaf 2 -SNP_pval 1e-6 -bam /home/macampbell2/pike-time/bamlists/197.txt
+#Do the calculations (putting in absolute paths). Making sure sites are present in 90% of individuals.
+/home/macampbell2/angsd/angsd -minInd 177 -GL 1 -out /home/macampbell2/pike-time/outputs/102/197 -nThreads 24 -doGlf 2 -doMajorMinor 1  -doMaf 2 -SNP_pval 1e-6 -bam /home/macampbell2/pike-time/bamlists/197.txt
 
 # Step 3
 # Generate a covariance matrix
