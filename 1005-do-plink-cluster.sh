810b8baea8f3faf772af7130a463fe677f3668a9
Matthew Campbell <macampbell2@chinook00.rcs.alaska.edu>
2020-01-09 14:38:35 -0900

    binary plink fails

--- 1005-do-plink-cluster.sh
+++ 1005-do-plink-cluster.sh
@@ -2,7 +2,7 @@
 
 #This one is to just make a single *.vcf file.
 
-#SBATCH -J plink-plink
+#SBATCH -J plink1
 
 #SBATCH --partition=bio
 #SBATCH --ntasks=24
@@ -18,8 +18,8 @@
 
 $HOME/angsd/angsd -P 24 -b $HOME/pike-time/bamlists/175.bamlist -minInd 157  -out $HOME/pike-time/outputs/1000/175-plink1 \
 -minMaf 0.05 -minMapQ 30 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 \
--SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -doPlink 1 > $HOME/pike-time/outputs/1000/plink.out \
-2> $HOME/pike-time/outputs/1000/plink.err
+-SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -doPlink 1 > $HOME/pike-time/outputs/1000/plink1.out \
+2> $HOME/pike-time/outputs/1000/plink1.err
 
 #Then maybe I can recode
 # https://www.cog-genomics.org/plink/1.9/data#recode
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --partition=debug
+#SBATCH --ntasks=24
+#SBATCH --tasks-per-node=24
+#SBATCH --mem=1G
+
+echo "Hello world"
+
