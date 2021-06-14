e59b79c96a2f6148b63c735af750be8abac0a16a
MacCampbell <campbellmatthewa@gmail.com>
2021-06-14 13:41:23 -0800

    Setting up covariance matrix, admixture proportions from 358 inds.

---
+++ 1201.1-calculate-pca.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+#SBATCH --partition=bio
+#SBATCH --ntasks=24
+#SBATCH --tasks-per-node=24
+#If running on the bio or analysis queue add:
+#SBATCH --mem=214G
+
+#SBATCH --mail-user=macampbell2@alaska.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+
+
+#Step one
+#Make sure we have directories
+mkdir /home/macampbell2/pike-time/outputs
+mkdir /home/macampbell2/pike-time/outputs/1201
+
+
+# Step two
+# The only input PCAngsd needs and accepts are genotype likelihoods in Beagle format.
+# ANGSD can be easily be used to compute genotype likelihoods and output them in the
+# required Beagle format {says the manual}.
+
+#Do the calculations (putting in absolute paths). Making sure sites are present in 90% of individuals.
+/home/macampbell2/angsd/angsd -P 24 -minInd 322 -GL 1 -out /home/macampbell2/pike-time/outputs/1201/358 -nThreads 24 -doGlf 2 -doMajorMinor 1 -doMaf 2 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/358.bamlist
+
+# Step 3
+# Generate a covariance matrix and calculate kinship coefficient
+# For Linux
+python /home/macampbell2/pcangsd/pcangsd.py -beagle /home/macampbell2/pike-time/outputs/1201/358.beagle.gz  -kinship -admix -o /home/macampbell2/pike-time/outputs/102/358 -threads 24
+
+# admixture proportions are in .qopt
+# covariance matrix is .cov
+# kinship matrix is .kinship.npy
+
+# Step 4 point to kinship and do it again
+python /home/macampbell2/pcangsd/pcangsd.py -beagle /home/macampbell2/pike-time/outputs/1201/358.beagle.gz  -relate /home/macampbell2/pike-time/outputs/102/358.kinship.npy  -admix -o /home/macampbell2/pike-time/outputs/102/358-relate -threads 24
