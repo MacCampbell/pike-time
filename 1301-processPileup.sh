156c6a8ab1faa03f5294009f330ff1dee0122ec6
MacCampbell <campbellmatthewa@gmail.com>
2021-07-05 11:08:12 -0800

    Using pileup to compute regions of coverage

--- 1301-processPileup.sh
+++ 1301-processPileup.sh
@@ -1,6 +1,18 @@
 #! /bin/bash 
 
 
+#SBATCH --partition=bio
+#SBATCH --ntasks=2
+#SBATCH --tasks-per-node=2
+#If running on the bio or analysis queue add:
+#SBATCH --mem=214G
+
+#SBATCH --mail-user=macampbell2@alaska.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+
 #Setting up for Chinook
 #It may be best to read in a bamlist, then do the processes and make the genome general.
 # 1301-processPileup.sh bamlist.bamlist genome.fasta
@@ -13,15 +25,19 @@ ref=$2
 #	do
 #		samtools mpileup -f /home/mac/data/OmyGenomeV06/omyV6Chr.fasta  $f > "`basename $f .sorted.bam`.pileup"
 #	done
+
+
 for line in $(cat ${1}) ;
-do
-  echo $line;
-  samtools mpileup -f ${2} $line > outputs/1300/"`basename $line .sorted.bam`.pileup"
+  do
+    echo $line;
+#Maybe we don't need all the columns, only 1-4 look useful to me now
+#In fact, samtools depth may do the same thing minus the base.
+    samtools mpileup -f ${2} $line | cut -f 1-4 > outputs/1300/"`basename $line .sorted.bam`.pileup"
 done
 
-#for f in *.pileup;
-#	do
-#		awk -v thresh=10 '$4 >= thresh {if(on==1) {sum+=$4; n++; next;}  start = $2; on = 1; sum=$4; n=1;} $4<thresh {if(on==0) next; on=0; print $1, start, $2, sum/n}' $f > "`basename $f .pileup`.dat"
-#	done
+for f in outputs/1300/*.pileup;
+  do
+	  	awk -v thresh=10 '$4 >= thresh {if(on==1) {sum+=$4; n++; next;}  start = $2; on = 1; sum=$4; n=1;} $4<thresh {if(on==0) next; on=0; print $1, start, $2, sum/n}' $f > "`basename $f .pileup`.dat"
+  done
 
 
