a22676e5025a7c0d2247550480790f60ae8953ff
MacCampbell <campbellmatthewa@gmail.com>
2021-07-23 11:04:33 -0800

    reducing files in wgs.bamlist

--- 1300.1-pca.sh
+++ 1300.1-pca.sh
@@ -13,7 +13,7 @@
 #SBATCH -o outputs/1300/1300.1.stdout
 
 #Generate beagle file
-/home/macampbell2/angsd/angsd -minInd 42 -GL 1 -out /home/macampbell2/pike-time/outputs/1300/wgs -nThreads 12 -doGlf 2 -doMajorMinor 1 -doMaf 2 \
+/home/macampbell2/angsd/angsd -minInd 41 -GL 1 -out /home/macampbell2/pike-time/outputs/1300/wgs -nThreads 12 -doGlf 2 -doMajorMinor 1 -doMaf 2 \
 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/wgs.bamlist -rf metadata/regions.tsv
 
 #Generate covariance matrix
