e40dcaaccd6ea455c0a913834a9826a614c098de
maccampbell <campbellmatthewa@gmail.com>
2021-08-20 13:52:34 -0800

    Redoing SC bamlist

--- 1401.1-beagle.sh
+++ 1401.1-beagle.sh
@@ -14,11 +14,13 @@
 #SBATCH -o outputs/1401/1401.1.stdout
 
 #Generate beagle file
-/home/macampbell2/angsd/angsd -minInd 155 -GL 1 -out /home/macampbell2/pike-time/outputs/1401/sc -nThreads 12 -doGlf 2 -doMajorMinor 1 -doMaf 2 -minMaf 0.05 \
--SNP_pval 1e-5 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/pike_bams_list_all.txt
+#cat pike_bams_list.txt | perl -pe 's/\/Users\/mchale\/Desktop\/pike_//g' > sc.bamlist.txt
+
+/home/macampbell2/angsd/angsd -minInd 172 -GL 1 -out /home/macampbell2/pike-time/outputs/1401/sc -nThreads 12 -doGlf 2 -doMajorMinor 1 -doMaf 2 -minMaf 0.05 \
+-SNP_pval 1e-5 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/sc.bamlist.txt
 
 
-#Generate covariance matrix
+##Generate covariance matrix
 python /home/macampbell2/pcangsd/pcangsd.py -beagle /home/macampbell2/pike-time/outputs/1401/sc.beagle.gz -kinship -admix -selection -sites_save -snp_weights -o /home/macampbell2/pike-time/outputs/1401/sc-pca -threads 10
 
 #Do again with related individuals
