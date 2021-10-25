0db28ef496dc91bf2b993763b14621ab8bc80d35
maccampbell <campbellmatthewa@gmail.com>
2021-10-25 11:31:38 -0800

    Adding do geGeno 4 to plink calls

--- data-sets-dapc.sh
+++ data-sets-dapc.sh
@@ -17,24 +17,24 @@
 # (1) data set of all samples 358 samples, 286.4 ~ 287 at 80 % missing, 95% post cutoff 
 /home/macampbell2/angsd/angsd -GL 2 -nThreads 8 -doGlf 2 -doMajorMinor 1 -doMaf 1 -minInd 287 \
 -minMaf 0.05 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/358.bamlist \
--out /home/macampbell2/pike-time/outputs/data/358-dapc -doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95
+-out /home/macampbell2/pike-time/outputs/data/358-dapc -doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95 -doGeno 4
 
 # (2) Southcentral data set
 
 /home/macampbell2/angsd/angsd -minInd 172 -GL 2 -out /home/macampbell2/pike-time/outputs/data/sc-dapc -nThreads 12 -doGlf 2 \
 -doMajorMinor 1 -doMaf 1 -minMaf 0.05 \
 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/sc.bamlist.txt \
--doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95
+-doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95 -doGeno 4
 
 # (3) Anchorage subset
 /home/macampbell2/angsd/angsd -GL 2 -nThreads 8 -doGlf 2 -doMajorMinor 1 -doMaf 1 -minInd 68 \
 -minMaf 0.05 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/anchorage.bamlist \
 -out /home/macampbell2/pike-time/outputs/data/anchorage-dapc \
--doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95
+-doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95 -doGeno 4
 
 # (4) Susitna subset
 /home/macampbell2/angsd/angsd -GL 2 -nThreads 8 -doGlf 2 -doMajorMinor 1 -doMaf 1 -minInd 78 \
 -minMaf 0.05 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -bam /home/macampbell2/pike-time/bamlists/susitna.bamlist \
 -out /home/macampbell2/pike-time/outputs/data/susitna-dapc \
--doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95
+-doPlink 2 -doCounts 1 -doPost 1 postCutoff 0.95 -doGeno 4
 
